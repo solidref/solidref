c2d3bfa69de9c73286242b058073f73d92b9af7d
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2024-02-28 19:07:08 +0200

    chore: fixed icon dynamic loading

--- src/views/pages/language/LanguageHero.tsx
+++ src/views/pages/language/LanguageHero.tsx
@@ -1,15 +1,8 @@
-import React, {useState, useEffect, lazy, Suspense} from 'react';
 import {Box, Typography, useTheme, useMediaQuery} from '@mui/material';
 
 import Hero from '../../generic/Hero';
 import {Language} from '../../../state';
-import GenericCodeIcon from '../../icons/GenericCodeIcon';
 import LazyLoadIcon from '../../../components/icons/LazyLoadIcon';
-
-type PresentableLanguage = {
-  language: Language;
-  logoComponent: React.ElementType | null;
-};
 
 export type LanguageHeroProps = {
   language: Language;
@@ -21,43 +14,8 @@ export default function LanguageHero({language}: LanguageHeroProps) {
     defaultMatches: true,
   });
 
-  // version 2
-  // const fullLanguage: PresentableLanguage = {
-  //   language,
-  //   logoComponent: lazy(async () => {
-  //     const iconName = language.code.charAt(0).toUpperCase() + language.code.slice(1);
-  //     try {
-  //       return await import(`../../icons/${iconName}`);
-  //     } catch (e) {
-  //       console.error(`Error importing language icon: ${iconName}`, e);
-  //     }
-  //     return await import('../../icons/GenericCodeIcon');
-  //   }),
-  // };
-
-  // version 1
-  // const [fullLanguage, setFullLanguage] = useState<PresentableLanguage | null>(null);
-
-  // useEffect(() => {
-  //   const loadIcon = async () => {
-  //     const moduleName = language.code.charAt(0).toUpperCase() + language.code.slice(1);
-  //     try {
-  //       // // console.log(`../../icons/${moduleName}`);
-  //       // const module = await import(`../../icons/${moduleName}.js `);
-  //       // // console.log(module);
-
-  //       // setFullLanguage({ language, logoComponent: module.default as React.ElementType });
-  //       setFullLanguage({ language, logoComponent: lazy(() => import(`../../icons/${moduleName}`)) })
-  //     } catch (error) {
-  //       console.error(`Error importing language icon: ${moduleName}`, error);
-  //     }
-  //   };
-  //   loadIcon();
-  // }, [language, setFullLanguage]);
-
   return (
     <Hero breakpoints={{md: 12}}>
-      {/* {fullLanguage && ( */}
       <Box
         maxWidth={'100%'}
         data-aos={isMd ? 'fade-left' : 'fade-up'}
@@ -75,7 +33,6 @@ export default function LanguageHero({language}: LanguageHeroProps) {
         <Typography variant="h6">
           Here are the <i>{language.name}</i> Coding Principles or Design Patterns explained
         </Typography>
-        {/* version 3 */}
         <LazyLoadIcon
           icon={language.code.charAt(0).toUpperCase() + language.code.slice(1)}
           style={{
@@ -95,57 +52,7 @@ export default function LanguageHero({language}: LanguageHeroProps) {
             },
           }}
         />
-        {/* version 2 */}
-        {/* {[fullLanguage].map(({language, logoComponent: DynamicLanguageIcon}) => (
-            <Suspense
-              fallback={
-                <GenericCodeIcon
-                  style={{
-                    width: '8rem',
-                    height: '8rem',
-                    display: 'block',
-                    margin: 'auto',
-                    transform: 'rotate(-15deg) translateY(40px)',
-                  }}
-                />
-              }
-              key={language.code}
-            >
-              <DynamicLanguageIcon
-                style={{
-                  width: '8rem',
-                  height: '8rem',
-                  display: 'block',
-                  margin: 'auto',
-                  transform: 'rotate(-15deg) translateY(40px)',
-                }}
-              />
-            </Suspense>
-          ))} */}
-        {/* version 1 */}
-        {/* {fullLanguage !== null && fullLanguage.logoComponent !== null ? (
-            React.createElement(fullLanguage.logoComponent, {
-              style: {
-                width: '8rem',
-                height: '8rem',
-                display: 'block',
-                margin: 'auto',
-                transform: 'rotate(-15deg) translateY(40px)',
-              },
-            })
-          ) : (
-            <GenericCodeIcon
-              style={{
-                width: '8rem',
-                height: '8rem',
-                display: 'block',
-                margin: 'auto',
-                transform: 'rotate(-15deg) translateY(40px)',
-              }}
-            />
-          )} */}
       </Box>
-      {/* )} */}
     </Hero>
   );
 }
--- src/views/pages/languages/LanguagesList.tsx
+++ src/views/pages/languages/LanguagesList.tsx
@@ -1,18 +1,11 @@
-import React, {useEffect, useMemo, useState} from 'react';
+import {useMemo} from 'react';
 
 import {Box, Card, CardContent, Grid, Link, RegularBreakpoints, Typography} from '@mui/material';
 import {useRecoilValue} from 'recoil';
 import {HierarchyLanguage} from '../../../state';
-import GenericCodeIcon from '../../icons/GenericCodeIcon';
 import {generateLanguagePath} from '../../../utils/url';
 import {loadLanguageHierarchy} from '../../../selector';
 import LazyLoadIcon from '../../../components/icons/LazyLoadIcon';
-
-// type PresentableLanguage = {
-//   language: HierarchyLanguage;
-//   logoComponent: React.ElementType | null;
-// };
-
 type FilterMethod = (...args: unknown[]) => (value: HierarchyLanguage) => boolean;
 
 type Filters = Record<string, FilterMethod>;
@@ -54,50 +47,9 @@ export default function LanguagesList({
     return languagesHierarchy.list.filter(filterCb(...filterArgs));
   }, [languagesHierarchy.list, filterMode, filterArgs]);
 
-  // version 1
-  // // Assuming `filters` is an object with filtering functions you've defined elsewhere
-  // // and `FilterMethod` is a function type for these filters
-  // const filteredLanguages = useMemo(() => {
-  //   if (!languagesHierarchy.list) return [];
-
-  //   const filterCb = filters[`${filterMode}Filter`]; // Assuming this is a function
-  //   // console.log(`${filterMode}Filter`);
-  //   return languagesHierarchy.list.filter(filterCb(...filterArgs));
-  // }, [languagesHierarchy.list, filterMode, filterArgs]);
-
-  // const [languages, setLanguages] = useState<PresentableLanguage[]>([]);
-
-  // useEffect(() => {
-  //   const loadIcons = async () => {
-  //     const loadedLanguages = await Promise.all(
-  //       filteredLanguages.map(async (language) => {
-  //         const moduleName = language.code.charAt(0).toUpperCase() + language.code.slice(1);
-  //         try {
-  //           // Dynamically import icon based on the language code
-  //           // console.log(`../../icons/${moduleName}`);
-  //           const module = await import(`../../icons/${moduleName}.js`);
-  //           // console.log(module);
-
-  //           return {language, logoComponent: module.default};
-  //         } catch (error) {
-  //           console.error(`Error importing language icon: ${moduleName}`, error);
-  //         }
-  //         return {language, logoComponent: null};
-  //       }),
-  //     );
-
-  //     setLanguages(loadedLanguages);
-  //   };
-
-  //   loadIcons();
-  // }, [filteredLanguages]);
-
-  // console.log(languages.map(({ logoComponent }) => logoComponent));
-
   return (
     <Grid container spacing={4}>
       {languages.map((language) => (
-        // {languages.map(({ language, logoComponent }) => ( // version 1
         <Grid item xs={xs} sm={sm} md={md} key={`languages-grid-item-${language.code}`}>
           <Box component={Card} boxShadow={3} borderRadius={4}>
             {/* TODO: must add dropdown for child languages */}
@@ -133,28 +85,6 @@ export default function LanguagesList({
                     },
                   }}
                 />
-                {/* version 1 */}
-                {/* {logoComponent !== null ? (
-                  React.createElement(logoComponent, {
-                    style: {
-                      width: '8rem',
-                      height: '8rem',
-                      display: 'block',
-                      margin: 'auto',
-                      transform: 'rotate(-15deg)',
-                    },
-                  })
-                ) : (
-                  <GenericCodeIcon
-                    style={{
-                      width: '8rem',
-                      height: '8rem',
-                      display: 'block',
-                      margin: 'auto',
-                      transform: 'rotate(-15deg)',
-                    }}
-                  />
-                )} */}
                 <Typography
                   variant={'h6'}
                   gutterBottom
