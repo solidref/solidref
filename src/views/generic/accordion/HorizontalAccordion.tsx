c0972802ca543379c7e7cbc904f0bc4b70bf6c9b
Dragos Cirjan <dragos.cirjan+github2@gmail.com>
2024-03-04 01:01:00 +0200

    chore: fixed slider size example

--- src/views/generic/accordion/HorizontalAccordion.tsx
+++ src/views/generic/accordion/HorizontalAccordion.tsx
@@ -1,13 +1,13 @@
 // import * as React from 'react';
 
-import {styled} from '@mui/system';
-import {Accordion} from '@mui/material';
+import { styled } from '@mui/system';
+import { Accordion } from '@mui/material';
 
 const HorizontalAccordion = styled(Accordion)(() => ({
   // border: '1px green solid',
   borderRadius: '0px',
   flex: '0 1 auto',
-  height: '400px',
+  minHeight: '400px',
   transition: 'flex 0.3s ease',
   width: '3rem',
   alignItems: 'start',
